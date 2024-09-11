99e3f3071096a01cde3847834c44e3dd9296b63b
Mirr2 <mirr2.jo@gmail.com>
2024-09-11 15:15:19 +0900

    이행 여부 판단 ui 수정

--- src/views/charts/Charts.vue
+++ src/views/charts/Charts.vue
@@ -59,7 +59,12 @@ function submitDefect() {
         </CInputGroup>
       </CForm>
       <div class="search-results-1">
-        <p>{{ specificOutput }}</p>
+        <div class="title-section">
+          <h5>1.1.1.1 세부 점검 항목</h5>
+        </div>
+        <div class="content-section">
+          <p>{{ specificOutput }}</p>
+        </div>
       </div>
       <div class="search-results-2">
         <h5 class="document-title">관련 문서 목록</h5> 
@@ -311,8 +316,8 @@ export default {
     filteredResults() {
       if (this.searchQuery === '1.1.1.1') {
         return [
-          { id: 1, name: '정보보호 및 개인정보보호 보고 체계', content: incidentResponse },
-          { id: 2, name: '정보보호계획 및 내부 관리계획', content: securityPlan }
+          { id: 1, name: '정보보호 및 개인정보보호 보고 체계', content: incidentResponse, percentage: 94 },
+          { id: 2, name: '정보보호계획 및 내부 관리계획', content: securityPlan, percentage: 85 },
         ];
       }
       return this.results.filter(result =>
@@ -413,16 +418,25 @@ export default {
 }
 
 .search-results-1 {
+  display: flex;
+  flex-direction: column; /* 항목들을 세로로 나열 */
+  align-items: center; /* 항목들을 가운데 정렬 */
+  justify-content: center; /* 컨텐츠를 중앙 정렬 */
   margin-top: 10px;
-  height: 200px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   padding: 20px;
   background-color: white;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-align: left;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  height: auto; /* 높이 자동 조절로 변경 */
 }
+
+.title-section {
+  width: 100%; /* 전체 너비를 차지하도록 설정 */
+  text-align: center; /* 텍스트 가운데 정렬 */
+  border-bottom: 1px solid #e6e6e6; /* 회색 구분선 추가 */
+  padding-bottom: 10px; /* 구분선 아래 패딩 추가 */
+  margin-bottom: 20px; /* 내용과의 간격 추가 */
+}
+
 
 .button-container {
   display: flex;
